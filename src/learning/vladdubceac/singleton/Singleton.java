6a2510b7ec7e961d7d5ee24c4a52cc3c9729fa43
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-02-14 17:59:03 +0200

    Fixed constructor

--- src/learning/vladdubceac/singleton/Singleton.java
+++ src/learning/vladdubceac/singleton/Singleton.java
@@ -8,7 +8,7 @@ public class Singleton {
     // an instance attribute
     private int data = 0;
 
-    public Singleton() {
+    private Singleton() {
     }
 
     // by adding the synchronized keyword to getInstance(),
